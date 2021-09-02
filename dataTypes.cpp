e47e8bf4af455690dbf4a729870ff041811dcc36
Yashashwini Dixit <64366760+YashashwiniDixit@users.noreply.github.com>
2021-09-03 00:18:55 +0530

    learnt

---
+++ dataTypes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    float b;
+    char c;
+    bool d;
+    double e;
+    short int si;
+    long int li;
+    cout<<"size of int "<<sizeof(a)<<endl;
+    cout<<"size of float "<<sizeof(b)<<endl;
+    cout<<"size of char "<<sizeof(c)<<endl;
+    cout<<"size of bool "<<sizeof(d)<<endl;
+    cout<<"size of double "<<sizeof(e)<<endl;
+    cout<<"size of shortint "<<sizeof(si)<<endl;
+    cout<<"size of longint "<<sizeof(li)<<endl;
+
+    return 0;
+}
